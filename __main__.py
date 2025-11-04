61a9d462ebf9464a143e50a92bc089253ed1bf55
Somanith Preap <somanithpreap@gmail.com>
2025-11-04 20:43:20 +0700

    Completed with all features

--- __main__.py
+++ __main__.py
@@ -1,106 +1,140 @@
 import pygame
-from random import randint
+import time
 
-# Colors RGB values
-BLACK = (0, 0, 0)
-YELLOW = (255, 255, 0)
-PURPLE = (255, 0, 255)
-SKY_BLUE = (200, 255, 255)
-RED = (255, 0,0)
-GREEN = (0,255, 0)
+pygame.init()
 
-pygame.init()   # Setup/initalize PyGame
+'''creating the program window'''
+back = (200, 255, 255) #background color
+mw = pygame.display.set_mode((500, 500)) #main window
+mw.fill(back)
+clock = pygame.time.Clock()
 
-# Setup the background
-window = pygame.display.set_mode((500, 500))
-window.fill(SKY_BLUE)
-
-clock = pygame.time.Clock() # Create a game timer
-
+'''rectangle class'''
 class Area():
-    def __init__(self, x=0, y=0, width=0, height=0, color=None):
-        self.rect = pygame.Rect(x, y, width, height)
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, width, height) #rectangle
         self.fill_color = color
-
-    def fill(self): # create one ractangle
-        pygame.draw.rect(window, self.fill_color, self.rect)
-
-    def outline(self, frame_color, thickness):
-        pygame.draw.rect(window, frame_color, self.rect, thickness)
-
     def color(self, new_color):
         self.fill_color = new_color
-
-    def is_collide(self, x, y):
+    def fill(self):
+        pygame.draw.rect(mw, self.fill_color, self.rect)
+    def outline(self, frame_color, thickness): #outline of an existing rectangle
+        pygame.draw.rect(mw, frame_color, self.rect, thickness)     
+    def collidepoint(self, x, y):
         return self.rect.collidepoint(x, y)
-    
-
+   
+'''class label'''
 class Label(Area):
-    def set_text(self, text, fsize=20, text_color=(0,0,0)):
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
         self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
-
     def draw(self, shift_x=0, shift_y=0):
         self.fill()
-        window.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
 
-# Create 4 cards and store it in a list
+RED = (255, 0, 0)
+GREEN = (0, 255, 51)
+YELLOW = (255, 255, 0)
+DARK_BLUE = (0, 0, 100)
+BLUE = (80, 80, 255)
+LIGHT_GREEN = (200, 255, 200)
+LIGHT_RED = (250, 128, 114)
 cards = []
 num_cards = 4
+x = 70
+
+start_time = time.time()
+cur_time = start_time
+
+''' Game Interface'''
+
+time_text = Label(0,0,50,50,back)
+time_text.set_text('Time:',40, DARK_BLUE)
+time_text.draw(20, 20)
+
+timer = Label(50,55,50,40,back)
+timer.set_text('0', 40, DARK_BLUE)
+timer.draw(0,0)
+
+score_text = Label(380,0,50,50,back)
+score_text.set_text('Count:',45, DARK_BLUE)
+score_text.draw(20,20)
+
+score = Label(430,55,50,40,back)
+score.set_text('0', 40, DARK_BLUE)
+score.draw(0,0)
+
 for i in range(num_cards):
-    card = Label(60 + i*100, 200, 75, 100, YELLOW)
-    card.outline(PURPLE, 10)
-    card.set_text('CLICK', 26)
-    cards.append(card)
+     new_card = Label(x, 170, 70, 100, YELLOW)
+     new_card.outline(BLUE, 10)
+     new_card.set_text('CLICK', 26)
+     cards.append(new_card)
+     x = x + 100
 
-points_label = Label(425, 10, 100, 50, SKY_BLUE)
-points_label.set_text('Points:', 35)
-points_num = Label(450, 32, 100, 50, SKY_BLUE)
-points_num.set_text('0', 35)
-
-wait = 0 
-
+wait = 0
 points = 0
 
-''' game loop '''
+from random import randint
+
 while True:
-    # Check for events and updating stats
-    for event in pygame.event.get():
-        # if event.type == pygame.QUIT:
-        #     pygame.quit()
-        #     exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x,y = event.pos
-
-            for i in range(num_cards):
-                if cards[i].is_collide(x,y): 
-                    if i + 1 == click:  # if clicked on correct card
-                        points += 1
-                        cards[i].color( (0,255,0) )
-                    else:               # if wrong
-                        points -= 1
-                        cards[i].color( (255, 0,0))
-                    points_num.set_text(str(points), 35)
-                    cards[i].fill()
-
-    # for card in cards:
-    #     card.draw(12, 40)
-
+    '''Drawing cards and displaying clicks'''
     if wait == 0:
-        wait = 35
+        wait = 20 #how many ticks of the label will be in one place
         click = randint(1, num_cards)
         for i in range(num_cards):
             cards[i].color(YELLOW)
-
             if (i + 1) == click:
                 cards[i].draw(10, 40)
             else:
                 cards[i].fill()
     else:
         wait -= 1
+    '''Handling clicks on cards'''
+    for event in pygame.event.get():
+        # Uncomment if running locallaly
+        # if event.type == pygame.QUIT:
+        #     pygame.quit()
+        #     exit()
 
-    points_label.draw()
-    points_num.draw()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = event.pos
+            for i in range(num_cards):
+                #looking for the card that the click hit
+                if cards[i].collidepoint(x,y):
+                    if i + 1 == click: # if there is a label on the card, we color it green, add a point
+                        cards[i].color(GREEN)
+                        points += 1
+                    else: #otherwise color it red, minus a point
+                        cards[i].color(RED)
+                        points -= 1
+                    cards[i].fill()
+                    score.set_text(str(points),40, DARK_BLUE)
+                    score.draw(0,0)
 
-    clock.tick(40)  # Limit to 40 frames per second
-    pygame.display.update() # Update the display
+    '''Winning and losing'''
+    new_time = time.time()
+ 
+    if new_time - start_time >= 11:
+        win = Label(0, 0, 500, 500, LIGHT_RED)
+        win.set_text("Time's up!!!", 60, DARK_BLUE)
+        win.draw(110, 180)
+        break
+
+    if int(new_time) - int(cur_time) == 1: # update time every second
+        timer.set_text(str(int(new_time - start_time)),40, DARK_BLUE)
+        timer.draw(0,0)
+        cur_time = new_time
+
+    if points >= 5:
+        win = Label(0, 0, 500, 500, LIGHT_GREEN)
+        win.set_text("You won!!!", 60, DARK_BLUE)
+        win.draw(140, 180)
+        resul_time = Label(90, 230, 250, 250, LIGHT_GREEN)
+        resul_time.set_text("Completion time: " + str (int(new_time - start_time)) + " sec", 40, DARK_BLUE)
+
+        resul_time.draw(0, 0)
+        break
+ 
+    pygame.display.update()
+    clock.tick(40)
+
+pygame.display.update()
